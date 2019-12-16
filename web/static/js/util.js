bde7ba15547b072552cb3baabf654bd0d41a5abd
lym <luoym@gktsingchip.com>
2019-12-16 09:20:44 +0800

    1、新增Qs.js。 2、修改封装的axios函数。 3、修改发送图片是的js

--- web/static/js/util.js
+++ web/static/js/util.js
@@ -1,17 +1,56 @@
-// http request拦截器 添加一个请求拦截器
-axios.interceptors.request.use(function (config) {
-    // Do something before request is sent
-    let token = window.localStorage.getItem("token")
-    if (token) {
-        config.headers.accessToken = token;    //将token放到请求头发送给服务器
+// // http request拦截器 添加一个请求拦截器
+// axios.interceptors.request.use(function (config) {
+//     // Do something before request is sent
+//     let token = window.localStorage.getItem("token")
+//     if (token) {
+//         config.headers.accessToken = token;    //将token放到请求头发送给服务器
+//         config.headers.contentType = 'application/json';
+//         return config;
+//         //这里经常搭配token使用，将token值配置到tokenkey中，将tokenkey放在请求头中
+//         // config.headers['accessToken'] = Token;
+//     }
+// }, function (error) {
+//     // Do something with request error
+//     return Promise.reject(error);
+// });
+axios.interceptors.request.use(
+    config => {
+        // const token = getCookie('名称');注意使用的时候需要引入cookie方法，推荐js-cookie
+        config.data = JSON.stringify(config.data);
+        let token = window.localStorage.getItem("token")
+        config.headers = {
+            'Content-Type':'application/json',
+            'accessToken' : token
+        }
+        // if(token){
+        //   config.params = {'token':token}
+        // }
         return config;
-        //这里经常搭配token使用，将token值配置到tokenkey中，将tokenkey放在请求头中
-        // config.headers['accessToken'] = Token;
+    },
+    error => {
+        return Promise.reject(err);
     }
-}, function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-});
+)
+// var commonUrl="http://localhost:8080/eslsystem/";
+// function sendAxios(url,data = {}){
+//     var encrypt = new JSEncrypt();
+//     encrypt.setPublicKey("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCaVvWsXQoGZCfZk1snIWivx0JGE3UQCzQfT7OJ" +
+//         "APDqBEKyslrM8ri4dlR9sVdcH1nzWR3XTk1reA0ATG4WUI+R1RaLp6+m6TNFxRPSibarFAZGryAQal4JEEN4q5G0KxwWvasF82G6epZTz7X" +
+//         "SPIqHywFTrR3RuEb0SGDbSurqkQIDAQAB");
+//     var nowtime = Date.parse(new Date());
+//     var mykey = "zssb" +"|"+ nowtime;
+//     var m = encrypt.encrypt(mykey);
+//     data.passSign = m;
+//     return new Promise((resolve,reject) => {
+//         axios.post(commonUrl+url,data)
+//             .then(response => {
+//                 resolve(response.data);
+//             },err => {
+//                 reject(err)
+//             })
+//     })
+// }
+
 /*封装发送axios的方法*/
 var commonUrl="http://localhost:8080/eslsystem/";
 function sendAxios(myUrl,myData){
@@ -26,7 +65,8 @@ function sendAxios(myUrl,myData){
     var mykey = "zssb" +"|"+ nowtime;
     var m = encrypt.encrypt(mykey);
     //向数据中添加签名
-    myData.passSign = m;
+    console.log(myData)
+    // myData.passSign = m;
     let token = sessionStorage.getItem("token")
     if (token !=null && token != "")this.Authorization = token;
     return new Promise((resolve, reject) => {
@@ -34,10 +74,11 @@ function sendAxios(myUrl,myData){
         const instance = axios.create({
             method : "POST", //请求方式
             url : commonUrl+myUrl, //请求地址
-            params : myData,
+            data :  myData,
             headers: {
+                'Authorization':token,
                 'Content-Type': 'application/json',
-                'Authorization':token
+                'passSign': m
             }
 
         });
@@ -50,3 +91,7 @@ function sendAxios(myUrl,myData){
         })
     })
 }
+
+
+
+
